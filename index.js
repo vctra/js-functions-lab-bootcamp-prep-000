892f06a169aa6cdd9a4645553d2d170841be9ad1
Victoria Hong <victoriaxhong@gmail.com>
2018-05-18 20:10:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 function happyHolidays() {
-  return "Happy holidays!" }
+  console.log "Happy holidays!" }
 
 function happyHolidaysTo(name) {
   return 'Happy holidays, ${name}!' }
