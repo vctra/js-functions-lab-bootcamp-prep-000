64641fd5ccdf404394ff8a653e94938c0c25ec99
Victoria Hong <victoriaxhong@gmail.com>
2018-05-18 20:16:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 // write your code below!
-function happyHolidays() {
-  console.log('Happy holidays!') }
+function happyHolidays() { return "Happy holidays!"; }
 
-function happyHolidaysTo() { console.log('Happy holidays, ${name}!') }
+function happyHolidaysTo(name) { return `Happy holidays, ${name}!`; }
 
-function happyHolidayTo(holiday, name) {
-  console.log('Happy ${holiday}, ${name}!') }
+function happyHolidayTo(holiday, name) { return `Happy ${holiday}, ${name}!`; }
 
-function holidayCountdown(days, holiday) { console.log('It's ${days} days until ${holiday}!') }
+function holidayCountdown(holiday, number) { return `It's ${number} days until ${holiday}!`; }
